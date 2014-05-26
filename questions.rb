70d5ac93dd2ebf5b4808e2bab1eaae1c62569d5b
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-26 16:21:57 +0100

    8 tests pass

--- questions.rb
+++ questions.rb
@@ -29,20 +29,30 @@ end
 # [['Bob', 'Clive'], ['Bob', 'Dave'], ['Clive', 'Dave']]
 # make sure you don't have the same pairing twice, 
 def every_possible_pairing_of_students(array)
+
 end
 
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  # SCM - both of these methods work:
+  array.drop(3)
+  # array.select { |e| array.index(e) > 2 }
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+  array.select { |e| e.reverse! }
+  array.sort!
+  array.select { |e| e.reverse! }
+  # last_char = x[-1,1]
+
 end
 
 # cut strings in half, and return the first half, e.g.
