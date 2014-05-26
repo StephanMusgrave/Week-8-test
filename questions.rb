5376378f6d756fb812ca4aa651277988bf2b4f31
Steve Musgrave <steve.musgrave@yahoo.co.uk>
2014-05-26 17:25:18 +0100

    11 tests pass

--- questions.rb
+++ questions.rb
@@ -59,11 +59,16 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  length = ((string.length.to_f/2).ceil)-1
+  string.slice!(0..length)
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  number = 0 - number if number > 0
+  else 
+  number 
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of 
@@ -71,6 +76,10 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  odd, even = [], []
+  even = array.select { |e| e.even?}
+  odd = array.select { |e| e.odd?}
+  array = [even,odd]
 end
 
 # count the numbers of elements in an element which are palindromes
